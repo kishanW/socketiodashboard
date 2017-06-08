07eab325a479e8c59e6dc7cd18f95694bc7c68b2
Kishan Wanigasinghe <kwanigasinghe@quicksolutions.com>
2017-06-08 14:42:26 -0400

    node file count stuff updated

--- npm-only/file-counter.js
+++ npm-only/file-counter.js
@@ -3,6 +3,8 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var lodash = require('lodash');
+var countFiles = require('count-files');
+var winFs = require('windows-fs');
 
 //resource wireup
 app.use("/css", express.static(__dirname + '/css'));
@@ -10,6 +12,9 @@ app.use("/css", express.static(__dirname + '/css'));
 // server config
 http.listen(3000, function(){
 	console.log('listening on *:3000');
+
+	var dirPath = "c:/temp";
+	var result = winFs.statDirectory(dirPath);
 });
 
 //routing
